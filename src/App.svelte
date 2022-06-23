b8e4fb75d59867803b0a6aa070105e3514890af0
Mikha Davids <mikhadavids@gmail.com>
2022-06-23 02:54:57 +0200

    MVP

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Card from "./lib/Card.svelte";
+	import Tile from "./lib/Tile.svelte";
+
+	const animations = [
+		"grow",
+		"shrink",
+		"spin",
+		"logo",
+		"logo infinite",
+		"oscillate",
+		"slide",
+		"slide chase",
+	];
+
+	let card: string | null;
+</script>
+
+<svelte:body on:keydown={(e) => e.key === "Escape" && (card = null)} />
+
+<main>
+	{#each animations as name}
+		<Tile {name} on:click={() => (card = name)} />
+	{/each}
+	{#if card}
+		<Card bind:animation={card} />
+	{/if}
+</main>
+
+<style>
+	main {
+		padding: 5rem;
+		display: flex;
+		flex-flow: row wrap;
+		align-content: flex-start;
+		justify-content: center;
+		gap: 2rem;
+	}
+</style>
---
+++ src/lib/Tile.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<div on:click>
+	<img src={`./animations/${name}.svg`} alt={name} />
+</div>
+
+<style lang="scss">
+	div {
+		width: 14rem;
+		padding: 2rem;
+		background-color: var(--bg-secondary);
+		aspect-ratio: 1;
+		border-radius: var(--br);
+		cursor: pointer;
+		transition: scale 0.2s ease;
+		&:hover {
+			scale: 1.1;
+		}
+	}
+</style>
